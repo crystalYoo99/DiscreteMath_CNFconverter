a70d57986b025fd176d7ec53980ad285837744a0
crystalYoo99 <21800464@handong.edu>
2019-11-09 00:45:13 +0900

    input

--- CNF_converter.c
+++ CNF_converter.c
@@ -1,22 +1,40 @@
+#define _CRT_SECURE_NO WARNINGS
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct{
     char element[10];
     struct node * next[100];
-    int level;
     int siblingCount;
 }node;
 
 int main(){
-
     //input receive
+    char input_str[100];
+    //int arraySize = 100;
+    char* input[100] = { NULL, };
+    int i = 0;
+
+    //int len = 0;
+    printf("input : ");
+    gets(input_str);
+
+    char* ptr = strtok(input_str, " ");
+
+    while(ptr != NULL) {
+      input[i] = ptr;
+      i++;
+      ptr = strtok(NULL, " ");
+    }
+
+    return 0;
+
 
     //make tree
-
     nnf();
     cnf();
     printResult();
-
 }
 
 node * makeSiblingLeaf(char newElement[], int siblingCount){
