e1fb5de88144a7c2fad86b223989bae0aefbd5b9
hanna <21800389@handon.edu>
2019-11-08 22:19:40 +0900

    뼈대 세우기

--- CNF_converter.c
+++ CNF_converter.c
@@ -3,21 +3,34 @@
 typedef struct{
     char element[10];
     struct node * next[100];
+    int level;
+    int siblingCount;
 }node;
-
-int level;
-int childCount;
 
 int main(){
 
+    //input receive
+
+    //make tree
+
+    nnf();
+    cnf();
+    printResult();
+
 }
 
-node * makeChildLeaf(char newElement[]){
+node * makeSiblingLeaf(char newElement[], int siblingCount){
     node*newNode = (node*)malloc(sizeof(node));
-    
     newNode->element[10]=newElement;
+    newNode->next[siblingCount]=NULL;
+    siblingCount++;
+    return newNode;
+}
 
-    newNode->next[childCount]=NULL;
+node * makeChildLeaf(char newElement[], int siblingCount){
+    node*newNode = (node*)malloc(sizeof(node));
+    newNode->element[10]=newElement;
+    newNode->next[siblingCount]=NULL;
     return newNode;
 }
 
